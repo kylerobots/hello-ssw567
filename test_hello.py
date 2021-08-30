9e86bf8631a93241ad77eb48391d62f6b957dd9e
Kyle M. Hart <kylemhart@gmail.com>
2021-08-30 17:41:33 -0400

    Add simple hello world test.

---
+++ test_hello.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestHello(unittest.TestCase):
+    """
+    @brief A class to test that I know how to use unittest.
+    """
+
+    def test_always_pass(self):
+        """
+        @test This always passes
+        """
+        self.assertTrue(True)
